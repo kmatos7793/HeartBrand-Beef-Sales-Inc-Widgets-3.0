2bdc82d5c2b053833fd86367ecebf9911116aaf3
AzureAD\KennethJohnMatos <kmatos@yotpo.com>
2025-04-28 22:23:50 +0800

    Rodan & Fields Widgets 3.0

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "554605";
+  let InstanceId_Widget = "1096396";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1091539";
+  let InstanceId_StarRating = "1096407";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "487266";
+  let ProductId = "";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/YWmK7tnEJ9sPd3aABUHVlkB6C0GwlIhuS1Zekw7K" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/Dr1qurQZN3QfGJDawKBbvWKeW8u8ncBAanZpKhkd" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
